a2da6b490ed199fb3104806c5dd6f52e033d3d32
Molly Clawson <mmclawson@icloud.com>
2021-12-07 15:03:39 -0500

    commiting everything

--- find-stocks.js
+++ find-stocks.js
@@ -2,12 +2,10 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://mollyclaw:Whiplash2015@cluster0.yqtdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
 var port = process.env.PORT || 3000;
 
+
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type':'text/html'});
-
-    res.write("Hello world");
-
 
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
@@ -21,9 +19,8 @@ http.createServer(function (req, res) {
             } 
             else 
             {
-              res.write("Items: ");
               for (i=0; i<items.length; i++)
-                  res.write(i + ": " + items[i].Company + " " + items[i].Ticker);				
+                  res.write(items[i].Company + " " + items[i].Ticker + "<br>");				
             }
             db.close();
             console.log("after close");
